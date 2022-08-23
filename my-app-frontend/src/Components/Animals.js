d53964299dd84a631b0fd3ac515ab8190d6f97f6
bandily <81126679+bandily@users.noreply.github.com>
2022-08-23 15:13:55 -0700

    wff, added rough cards, might scrap

--- my-app-frontend/src/Components/Animals.js
+++ my-app-frontend/src/Components/Animals.js
@@ -1,13 +1,30 @@
 import React from "react"
+import { Card } from "react-bootstrap"
 
-const Animals = () => {
-    return (
-        <>
-        <section className="hero">
-            <h1>Animals</h1>
-        </section>
-        </>
-    )
+const AnimalPage = () => {
+    const cardInfo = [
+        {image: "greger",title: "terwddwdqwdqwdqwdqwdqwter", text: "treter" },
+        {image: "fewfwe",title: "fwefwefwf", text: "fewfwefwef" },
+        {image: "fewfwefw",title: "fwefwefwe", text: "fewfwefwe" },
+        
+    ]
+
+    const Animals = (card, index) => {
+
+        return (
+            <>
+                <Card style={{ width: '18rem' }} key={index}>
+                    <Card.Img variant="top" src={card.image} />
+                    <Card.Body>
+                        <Card.Title>{card.title}</Card.Title>
+                        <Card.Text>{card.text}</Card.Text>
+                        {/* <Button variant="primary">Go somewhere</Button> */}
+                    </Card.Body>
+                </Card>
+            </>
+        )
+    }
+    return <div className="grid">{cardInfo.map(Animals)}</div>;
 }
 
-export default Animals
+export default AnimalPage
